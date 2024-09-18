b20fe4a3ab7ef763fc6a8a945569400d83f0be5c
Vladislav Kemenyash <kemenyash.vladislav@gmail.com>
2024-09-18 20:53:26 +0300

    Add project files.

---
+++ Lab1(Task-7)/Program.cs
@@ -0,0 +1,17 @@
+﻿Stack<string> operationSystems = new Stack<string>();
+operationSystems.Push("Windows");
+operationSystems.Push("Linux");
+operationSystems.Push("macOS");
+operationSystems.Push("Ubuntu");
+operationSystems.Push("Fedora");
+
+Console.WriteLine("Adding OS order:");
+operationSystems.Reverse().ToList().ForEach(os => Console.WriteLine(os));
+
+Console.WriteLine("\nReverse OS order:");
+operationSystems.ToList().ForEach(os => Console.WriteLine(os));
+
+Console.WriteLine($"\nOS count: {operationSystems.Count()}");
+
+operationSystems.Clear();
+Console.WriteLine("\nOS stack was cleared");
